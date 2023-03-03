cd01b161240a6f2f6758b716f8a102d420f15b96
Tarek Arar <tarek@rizek..com>
2023-03-03 14:33:54 +0400

    close icon

--- components/TrailerModal.jsx
+++ components/TrailerModal.jsx
@@ -4,6 +4,8 @@ import { WebView } from "react-native-webview";
 import { useQuery } from "react-query";
 import { MovieAPI } from "../apis/movieAPI";
 import Loading from "./Loading";
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const TrailerModal = ({ isOpen, close, movieId }) => {
   const { data, isLoading, error } = useQuery(["movie-videos", movieId], () =>
@@ -13,53 +15,47 @@ const TrailerModal = ({ isOpen, close, movieId }) => {
   const trailer = data?.data?.results?.find((el) => el.type == "Trailer");
 
   return (
-    <Modal
-      animationType="slide"
-      // transparent={true}
-      visible={isOpen}
-      onRequestClose={() => {
-        close();
-      }}
-    >
-      <View style={styles.centeredView}>
-        <Pressable
-          style={[styles.button, styles.buttonOpen]}
-          onPress={() => close()}
-        >
-          <Text style={styles.textStyle}>Close</Text>
+    <Modal animationType="slide" visible={isOpen} onRequestClose={close}>
+      <View style={styles.container}>
+        <Pressable onPress={close} style={styles.buttonClose}>
+          <AntDesign name="closecircle" size={24} color="black" />
         </Pressable>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <View style={styles.videoContainer}>
-            <WebView
-              source={{ uri: `https://www.youtube.com/embed/${trailer?.key}` }}
-            />
-          </View>
-        )}
+        <View style={styles.centeredView}>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <View style={styles.videoContainer}>
+              <WebView
+                source={{
+                  uri: `https://www.youtube.com/embed/${trailer?.key}`,
+                }}
+              />
+            </View>
+          )}
+        </View>
       </View>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22,
   },
   videoContainer: {
     height: 300,
     width: "100%",
   },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
+
   buttonClose: {
-    backgroundColor: "#2196F3",
+    marginTop: 60,
+    marginRight: 10,
+    alignSelf: "flex-end",
   },
   textStyle: {
     fontWeight: "bold",
